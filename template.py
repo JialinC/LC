24d328de1d00965e2ac48cbd2cce62866ce7b870
JialinC <cjl742952519@gmail.com>
2025-03-13 15:58:49 -0400

    add rotation logic

--- template.py
+++ template.py
@@ -21,3 +21,15 @@ while lo < hi:
 
 # Sorts indices based on the values in arr
 temp = sorted(range(n), key=lambda x: arr[x])
+
+
+# Rotate 90 degrees
+def rotate(self, matrix: List[List[int]]) -> None:
+    n = len(matrix[0])
+    for i in range(n // 2 + n % 2):
+        for j in range(n // 2):
+            tmp = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+            matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = matrix[i][j]
+            matrix[i][j] = tmp
